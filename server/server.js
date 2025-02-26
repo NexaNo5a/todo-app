7290e83115fd683b1bb9d00c7c6d44a74cc8c779
Younian <younian.zh@gmail.com>
2025-02-26 23:49:37 +1100

    backend apis

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
+
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const todoRoutes = require('./routes/todoRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(cors());
-app.use(bodyParser.json());
-const uri = "mongodb+srv://kyle:Zyn879544856@cluster-todo-app.jddizhq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-todo-app"
-mongoose.connect(uri)
+app.use(express.json());
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
-app.use('/api/todos', todoRoutes);
-
+app.use('/todos', todoRoutes);
+app.use('/auth', userRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+
 });
